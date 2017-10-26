a20215ffab6c07902f18a2bb0ce36d45ef347e3f
Jitesh Gupta <jiteshgupta1995@gmail.com>
2017-10-26 13:58:17 +0530

    sorting code added

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.listen(port, (err) => {
   console.log(`server is listening on ${port}`)
 });
 
-var byNameAsc = [
+var data = [
   ['Amelia','Dexter.Trantow57@hotmail.com','LN','12:00'],
   ['Estevan','Aimee7@hotmail.com','LosAngels','01:00'],
   ['Florence','Jarrod.Bernier13@yahoo.com','LasVegas','11:00'],
@@ -17,63 +17,28 @@ var byNameAsc = [
   ['Tressa','Yadira1@hotmail.com','NY','14:00']
 ];
 
-var byNameDesc = [
-  ['Tressa','Yadira1@hotmail.com','NY','14:00'],
-  ['Rylan','Angelita_Weimann42@gmail.com','PA','06:00'],
-  ['Florence','Jarrod.Bernier13@yahoo.com','LasVegas','11:00'],
-  ['Estevan','Aimee7@hotmail.com','LosAngels','01:00'],
-  ['Amelia','Dexter.Trantow57@hotmail.com','LN','12:00']
-];
-
-var byAddressAsc = [
-  ['Florence','Jarrod.Bernier13@yahoo.com','LasVegas','11:00'],
-  ['Amelia','Dexter.Trantow57@hotmail.com','LN','12:00'],
-  ['Estevan','Aimee7@hotmail.com','LosAngels','01:00'],
-  ['Tressa','Yadira1@hotmail.com','NY','14:00'],
-  ['Rylan','Angelita_Weimann42@gmail.com','PA','06:00']
-];
-
-var byAddressDesc = [
-  ['Rylan','Angelita_Weimann42@gmail.com','PA','06:00'],
-  ['Tressa','Yadira1@hotmail.com','NY','14:00'],
-  ['Estevan','Aimee7@hotmail.com','LosAngels','01:00'],
-  ['Amelia','Dexter.Trantow57@hotmail.com','LN','12:00'],
-  ['Florence','Jarrod.Bernier13@yahoo.com','LasVegas','11:00']
-];
-
-var byTimeAsc = [
-  ['Estevan','Aimee7@hotmail.com','LosAngels','01:00'],
-  ['Rylan','Angelita_Weimann42@gmail.com','PA','06:00'],
-  ['Florence','Jarrod.Bernier13@yahoo.com','LasVegas','11:00'],
-  ['Amelia','Dexter.Trantow57@hotmail.com','LN','12:00'],
-  ['Tressa','Yadira1@hotmail.com','NY','14:00']
-];
-
-var byTimeDesc = [
-  ['Tressa','Yadira1@hotmail.com','NY','14:00'],
-  ['Amelia','Dexter.Trantow57@hotmail.com','LN','12:00'],
-  ['Florence','Jarrod.Bernier13@yahoo.com','LasVegas','11:00'],
-  ['Rylan','Angelita_Weimann42@gmail.com','PA','06:00'],
-  ['Estevan','Aimee7@hotmail.com','LosAngels','01:00']
-];
+function sort(key, order){
+  data.sort(function (a, b) {
+    if(order == "asc" && key == "Name"){
+      return a[0].localeCompare(b[0]);
+    }else if(order == "desc" && key == "Name"){
+      return b[0].localeCompare(a[0]);
+    }else if(order == "asc" && key == "Address"){
+      return a[2].localeCompare(b[2]);
+    }else if(order == "desc" && key == "Address"){
+      return b[2].localeCompare(a[2]);
+    }else if(order == "asc" && key == "Time"){
+      return a[3].localeCompare(b[3]);
+    }else if(order == "desc" && key == "Time"){
+      return b[3].localeCompare(a[3]);
+    }
+  });
+}
 
 app.get('/fetchrecord', (req, res) => {
   var key = req.query.sortKey;
   var order = req.query.order;
   res.setHeader('Content-Type', 'application/json');
-  if(order == "asc" && key == "Name"){
-    res.send(byNameAsc).status(200);
-  }else if(order == "desc" && key == "Name"){
-    res.send(byNameDesc).status(200);
-  }else if(order == "asc" && key == "Address"){
-    res.send(byAddressAsc).status(200);
-  }else if(order == "desc" && key == "Address"){
-    res.send(byAddressDesc).status(200);
-  }else if(order == "asc" && key == "Time"){
-    res.send(byTimeAsc).status(200);
-  }else if(order == "desc" && key == "Time"){
-    res.send(byTimeDesc).status(200);
-  }else{
-    res.send(byNameAsc).status(200);
-  }
+  sort(key,order);
+  res.send(data).status(200);
 });
